1adaf3f3f833874d9bec920905e4b9406836c244
Teguh Prasetyo <teguhe@gmail.com>
2025-07-15 07:53:11 +0700

    convert angka ke text terbilang OK

--- delphi_terbilang_angka.dpr
+++ delphi_terbilang_angka.dpr
@@ -2,13 +2,13 @@ program delphi_terbilang_angka;
 
 uses
   Vcl.Forms,
-  umain in 'umain.pas' {Form1};
+  umain in 'umain.pas' {fMain};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TfMain, fMain);
   Application.Run;
 end.
