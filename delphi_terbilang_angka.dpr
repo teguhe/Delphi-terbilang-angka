8c7b056c6d3c98d61f3248720afbfa0040d726e7
Teguh Prasetyo <teguhe@gmail.com>
2025-07-15 07:48:55 +0700

    initial commit

---
+++ delphi_terbilang_angka.dpr
@@ -0,0 +1,14 @@
+program delphi_terbilang_angka;
+
+uses
+  Vcl.Forms,
+  umain in 'umain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
